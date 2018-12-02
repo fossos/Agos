901189e20e2a761a9466464b8a93adf408703922
Foss <45519098+fossos@users.noreply.github.com>
2018-12-02 18:48:32 +0100

    Updated AGBlocks.java
    Deleted " public static final BlockOre mirrorisOre = new BlockOre("mirroris_ore").setCreativeTab(AGTabs.tabBlock);" because he was useless

--- src/main/java/com/crypticmushroom/agos/block/AGBlocks.java
+++ src/main/java/com/crypticmushroom/agos/block/AGBlocks.java
@@ -5,5 +5,4 @@ import com.crypticmushroom.agos.client.AGTabs;
 public class AGBlocks {
 
     public static final BlockOre borelecOre = new BlockOre("borelec_ore").setCreativeTab(AGTabs.tabBlock);
-    public static final BlockOre mirrorisOre = new BlockOre("mirroris_ore").setCreativeTab(AGTabs.tabBlock);
-}
+   }   
